686e390aca07028bbee3f50a677e355a554105f2
kanbara <kanbara@bbtower.co.jp>
2015-01-30 16:15:13 +0900

    user edit no password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
   
   validates :username, presence: true, uniqueness: true
   
+  # allow users to update their accounts without passwords
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
