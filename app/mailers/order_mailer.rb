a68169e1ec5ed8b7099a1c4ba8095392c614a228
kanbara <kanbara@bbtower.co.jp>
2015-02-10 11:50:19 +0900

    fix order new mail and fix text_area line break print

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,6 +6,10 @@ class OrderMailer < ActionMailer::Base
     @owner = order.users[0].username
     @url = 'http://localhost:3000/orders/' + order.id.to_s
     
+    @contract = order.contract_number
+    @customer = order.customer_number
+    @comment = order.order_comment
+    
     mail to: tomail , subject: sub
   end
 end
