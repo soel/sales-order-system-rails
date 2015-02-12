2739ac0d031466bb42ee749a68b0a05eeebb3246
kanbara <kanbara@bbtower.co.jp>
2015-02-12 14:14:59 +0900

    add thumb_l avatar large

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -39,6 +39,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
      process :resize_to_fit => [20, 20]
   end
 
+  version :thumb_l do
+     process :resize_to_fit => [50, 50]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
