d7d855e87f2b306bd7b3c943e3d4f24329115f48
kanbara <kanbara@bbtower.co.jp>
2015-02-26 17:30:41 +0900

    order customer_number integer -> string

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,21 +1,21 @@
 class Order < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_and_belongs_to_many :destgroups
-  
+
   has_many :order_attachments
     accepts_nested_attributes_for :order_attachments, allow_destroy: true
-    
+
   has_many :comments
-  
+
   has_many :destemails, through: :destgroups
-  
-  validates :contract_number, numericality: { only_integer: true }, presence: true
-  validates :customer_number, numericality: { only_integer: true }, presence: true
-  
+
+  validates :contract_number, presence: true
+  validates :customer_number, presence: true
+
   validate :has_destgroups?
-  
+
   def has_destgroups?
     errors[:base] << "依頼先が設定されていません" if self.destgroups.blank?
   end
-  
+
 end
--- db/migrate/20150203025356_create_orders.rb
+++ db/migrate/20150203025356_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
       t.string :contract_number, null: false
-      t.integer :customer_number, null: false
+      t.string :customer_number, null: false
       t.date :delivery_date
       t.string :status, null: false
       t.text :web_url
